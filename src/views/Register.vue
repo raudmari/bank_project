21ecdf74afea3bf11a2f33b91c56455e8ef636c3
Marion <marion@marion.ee>
2021-04-27 15:55:14 +0300

    vue 27.04

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,7 +2,11 @@
   <div class="home">
     <input v-model="email" placeholder="enter your e-mail"/>
     <button v-on:click="register()">Register</button>
-
+    <table border="0.5">
+      <tr v-for="email in emails">
+        <td>{{email}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -12,12 +16,13 @@
 export default {
   data: function () {
     return {
-      'email': ''
+      'email': '',
+      'emails':[]
     }
   },
   methods: {
-    register() {
-      alert(this.email);
+    'register': function () {
+      this.emails.push(this.email);
     }
   }
 }
